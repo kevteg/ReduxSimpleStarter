c6237adbf4e996918375b6594c6749e7d05b4ac7
Kevin <kevteg05@gmail.com>
2017-03-06 15:45:12 -0400

    Classes components

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+//Classes components have the ability to save and stores info
+class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    //State is only on classes components
+    this.state = {term : ''}
+  }
+  render() {
+    //This has to return jsx
+    //To see changed state
+    //The idea is that the state is the one who changes the component
+    return (
+      <div>
+
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})} />
+      </div>
+    );
+  }
+}
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@
 import React from 'react';
 //This is what puts it in the dom
 import ReactDOM from 'react-dom';
-
+import SearchBar from './components/search_bar'
+const API_KEY = 'AIzaSyBJCW05AxjegIs66_YsJnxnQsKQYuqzFLE'
 //new component > new html
 //Const: ES6 sintax, declaring a variable
 //Const: constant, not changing
 const App = () => { //ES6 sintax
   //This is not html: JSX
   //JSX must be translated to vainilla js
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+);
 }
 //it nothings is inside: <App />
 //Put this generated html and put it in the page (page > DOM)
