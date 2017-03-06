40a5ea4c8a2becf600f0620ba43cad11cb4d168d
Kevin <kevteg05@gmail.com>
2017-03-06 00:46:01 -0400

    some initial practice on creating components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+//ONE COMPONENT PER FILE
 import React from 'react';
+//This is what puts it in the dom
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//new component > new html
+//Const: ES6 sintax, declaring a variable
+//Const: constant, not changing
+const App = () => { //ES6 sintax
+  //This is not html: JSX
+  //JSX must be translated to vainilla js
+  return <div>Hello world</div>;
+}
+//it nothings is inside: <App />
+//Put this generated html and put it in the page (page > DOM)
+//This creates an instance of App:<App />
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
